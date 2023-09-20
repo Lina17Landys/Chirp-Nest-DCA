741dedce144ae24f44cf24e389ba3329d8c1bff5
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-09-20 08:32:24 -0500

    fix

---
+++ src/components/signup/signup.ts
@@ -0,0 +1,42 @@
+console.log("hola");
+
+class SignUp extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot!.innerHTML = `
+    <link rel="stylesheet" href="../src/components/signup/login.css">
+
+    <div class="form-container">
+    <img class="cutImg" src="./img/signUpCut.png">
+    <div class="form-block">
+     
+      <form>
+      <button>Log in</button>
+
+        <h1>E-mail</h1>
+        <input type="email" placeholder="E-mail" />
+        <h1>User</h1>
+        <input type="password" placeholder="User name" />
+        <h1>Password</h1>
+        <input type="email" placeholder="Password" />
+        <h1>Repeat password</h1>
+        <input type="password" placeholder="Password" />
+
+       
+        <button class="btn">Create account</button>
+   
+  </div>
+  
+          `;
+  }
+}
+
+customElements.define("signup-container", SignUp);
