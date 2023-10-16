cac627b771e03a84dc3bd7b4672d897e6ea881a6
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-10-16 12:57:07 -0500

    exports update

--- src/components/export.ts
+++ src/components/export.ts
@@ -1,3 +1,7 @@
 export {default as SignUp} from "./signup/signup";
 export {default as Login} from "./login/login";
 //export {default as settingMenu} from "./settingMenu/settingMenu";
+//export {default as AccInfo} from "./smAccountInfo/accInfo";
+//export {default as Email} from "./accEmail/email";
+//export {default as Password} from "./password/password";
+//export {default as Deactivate} from "./deactivate/deactivate";
