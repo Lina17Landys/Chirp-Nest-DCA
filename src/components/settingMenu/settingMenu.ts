986321414e74a67fa8acf2b1c2e1426633f6a90c
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-10-16 10:41:13 -0500

    component

--- src/components/settingMenu/settingMenu.ts
+++ src/components/settingMenu/settingMenu.ts
@@ -1,19 +1,35 @@
 class settingMenu extends HTMLElement {
-    constructor() {
-        super()
-        this.attachShadow({mode:"open"})
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
-    connectedCallback() {
-        this.render();
-      }
+  connectedCallback() {
+    this.render();
+  }
 
-      render(){
-        this.shadowRoot!.innerHTML = `
+  render() {
+    this.shadowRoot!.innerHTML = `
         <link rel="stylesheet" href="setting.css">
-        
-        `
-      }
+        <div class="sidebar">
+                
+        <nav>
+            <ul class="navbar">
+            <li><img class="logo" src="../../../img/logo.png"></li>
+
+            <li><img src="../../../img/homeIcon.png"><a href="#">Home</a></li>
+            <li><img src="../../../img/searchIcon.png"><a href="#">Explore</a></li>
+            <li><img src="../../../img/bellIcon.png"><a href="#">Notifications</a></li>
+            <li><img src="../../../img/profileIcon.png"><a href="#">Profile</a></li>
+                <div class="user">
+                    <img src="pick.jpg" alt="Profile Picture">
+                    <button class="tweet-button">Tweet</button>
+                </div>
+            </ul>
+        </nav>
+    </div>
+        `;
+  }
 }
 customElements.define("setting-menu", settingMenu);
-export default settingMenu
+export default settingMenu;
