f77bff1f6d76f9c26665c92c64285a39db6d6895
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-10-19 12:06:29 -0500

    css

--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -23,18 +23,33 @@ class Profile extends HTMLElement {
             <li><img src="../../../img/bellIcon.png"><a href="#">Notifications</a></li>
             <li><img src="../../../img/profileIcon.png"><a href="#">Profile</a></li>
                 <div class="user">
-                    <img src="../../../img/profilePic.jpg" alt="Profile Picture">
+                    <img src="../../../img/profilePic.jpg" class="icon-small">
                     <button class="tweet-button">Tweet</button>
                 </div>
             </ul>
         </nav>
     </div>
 
+<section class="user">
+<img src="../../../img/banner.jpeg" class="banner-big">
+
+<img src="../../../img/profilePic.jpg" class="icon-big">
+<h1>Tricky</h1>
+<p>@Tricky</p>
+<button>Edit Profile</button>
+
+<p><img src="../../../img/calendarIcon.png">Joined on Oct 10</p>
+<p>Followers: 10</p>
+<p>Following: 3</p>
+
+<h2>Posts</h2>
+
+
+</section>
 
 
 
 
-    
         `;
       }
 }
