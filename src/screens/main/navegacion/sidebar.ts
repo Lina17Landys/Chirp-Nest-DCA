f84013c4685b04ce1e658ccc7c5ef650853e12ab
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-10-19 11:50:15 -0500

    profile base

--- src/screens/main/navegacion/sidebar.ts
+++ src/screens/main/navegacion/sidebar.ts
@@ -20,8 +20,6 @@ export default class Sidebar extends HTMLElement {
 
       this.shadowRoot!.innerHTML += `
     
-
-        
       <div class="sidebar">
                 
       <nav>
---
+++ src/screens/profile/profile.js
@@ -0,0 +1,43 @@
+class Profile extends HTMLElement {
+    constructor(){
+        super();
+        this.attachShadow({mode:"open"})
+    }
+
+    connectedCallback() {
+        this.render();
+      }
+
+      render(){
+        this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="./styles.css">
+
+        <div class="sidebar">
+                
+        <nav>
+            <ul class="navbar">
+            <li><img class="logo" src="../../../img/logo.png"></li>
+  
+            <li>    <img src="../../../img/homeIcon.png"<a href="#">Home</a></li>
+            <li><img src="../../../img/searchIcon.png"><a href="#">Explore</a></li>
+            <li><img src="../../../img/bellIcon.png"><a href="#">Notifications</a></li>
+            <li><img src="../../../img/profileIcon.png"><a href="#">Profile</a></li>
+                <div class="user">
+                    <img src="../../../img/profilePic.jpg" alt="Profile Picture">
+                    <button class="tweet-button">Tweet</button>
+                </div>
+            </ul>
+        </nav>
+    </div>
+
+
+
+
+
+    
+        `;
+      }
+}
+
+customElements.define("user-profile", Profile)
+export default Profile;
