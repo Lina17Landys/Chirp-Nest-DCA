681405e4521d102ed51cf368604e03eceaf39bd9
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-10-28 17:03:31 -0500

    file organization

---
+++ src/screens/dashboard/dasboard.ts
@@ -0,0 +1,50 @@
+import styles from "./styles.css";
+
+class dashboard extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    if (this.shadowRoot) {
+      this.shadowRoot.innerHTML = ``;
+
+      const css = this.ownerDocument.createElement("style");
+      css.innerHTML = styles;
+      this.shadowRoot?.appendChild(css);
+
+      this.shadowRoot!.innerHTML += `
+      <main style="display: flex; flex-direction: row; ">
+
+      <img src="./img/logo.png">
+
+      <div class="info-container">
+      <h1>Welcome to Chirp Nest!</h1>
+      <p>Within this platform you can express yourself freely, share content and interact with other people.
+      Join now to be part of this community!</p>
+
+      <button id="loginButton">Log in</button>
+      <button id="appleButton">Log in with Apple</button>
+      <div class="line">
+      <div class="linea"></div>
+      <div class="leyenda">Or</div>
+      <div class="linea"></div>
+    </div>
+      <button id="signUpButton">Create Account</button>
+<p>By signing up, you agree to the Terms of Service and Privacy Policy, including the Cookie Policy.</p>
+
+
+      </div>
+
+    `;
+    }
+  }
+}
+
+customElements.define("dashboard-container", dashboard);
+export default dashboard;
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,4 +1,4 @@
 export enum Screens {
     LOGIN = "LOGIN",
-    SIGNUP = "SIGNUP",
+    DASHBOARD = "DASHBOARD",
 }
