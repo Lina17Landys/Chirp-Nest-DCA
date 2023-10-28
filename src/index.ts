6abaac7ec4fe7af798d247de6df70b7f516aef0e
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-10-28 16:26:16 -0500

    css not working properly

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import styles from "./styles.css"
+import styles from "./styles.css";
 class Landing extends HTMLElement {
   constructor() {
     super();
@@ -11,15 +11,17 @@ class Landing extends HTMLElement {
 
   render() {
     if (this.shadowRoot) {
-      this.shadowRoot.innerHTML = ``
+      this.shadowRoot.innerHTML = ``;
 
-  const css = this.ownerDocument.createElement("style");
-  css.innerHTML = styles;
-  this.shadowRoot?.appendChild(css);
+      const css = this.ownerDocument.createElement("style");
+      css.innerHTML = styles;
+      this.shadowRoot?.appendChild(css);
 
       this.shadowRoot!.innerHTML += `
-      <link rel="stylesheet" href="./styles.css">
+      <main style="display: flex; flex-direction: row;">
+
       <img src="./img/logo.png">
+
       <div class="info-container">
       <h1>Welcome to Chirp Nest!</h1>
       <p>Within this platform you can express yourself freely, share content and interact with other people.
@@ -29,19 +31,19 @@ class Landing extends HTMLElement {
       <button id="appleButton">Log in with Apple</button>
       <div class="line">
       <div class="linea"></div>
-      <div class="leyenda">Or start with</div>
+      <div class="leyenda">Or</div>
       <div class="linea"></div>
     </div>
-
+      <button id="signUpButton">Create Account</button>
+<p>By signing up, you agree to the Terms of Service and Privacy Policy, including the Cookie Policy.</p>
 
 
       </div>
 
     `;
     }
-    }
   }
-
+}
 
 customElements.define("app-container", Landing);
 export default Landing;
